6c8e148b4901c0b40d53a5c9d5d7504b948812c8
Gregory Bowne <gbowne144@gmail.com>
2025-08-29 10:01:52 -0700

    Update jsonformatter.h
    fixing redefinition of lintIssue

--- jsonformatter.h
+++ jsonformatter.h
@@ -8,13 +8,6 @@
 void printIndent(std::ostream& os, int indent);
 void printJson(const std::shared_ptr<JsonValue>& value, std::ostream& os, int indent, int indentStep, bool compact);
 
-struct JsonLintIssue {
-    enum class Severity { Error, Warning, Info };
-    Severity severity;
-    std::string message;
-    int line, column;
-};
-
 std::vector<JsonLintIssue> lintJsonVector(const std::string& jsonStr);
 std::vector<JsonLintIssue> lintJson(const std::shared_ptr<JsonValue>& root);
 
