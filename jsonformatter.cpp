32c7d73fd0a7cf51fd28c3d143353e2acafa0196
Gregory Bowne <gbowne144@gmail.com>
2025-09-02 12:16:00 -0700

    Update jsonformatter.cpp
    remove redundant printIndent from formatter implementation

--- jsonformatter.cpp
+++ jsonformatter.cpp
@@ -1,9 +1,5 @@
 #include "jsonformatter.h"
 
-void printIndent(std::ostream& os, int indent) {
-    for (int i = 0; i < indent; ++i) os.put(' ');
-}
-
 void printJson(const std::shared_ptr<JsonValue>& value, std::ostream& os, int indent, int indentStep, bool compact) {
     if (!value) {
         os << "null";
